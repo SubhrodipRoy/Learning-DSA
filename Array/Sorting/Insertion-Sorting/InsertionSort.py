a54a1a5e4256537adc438359e812775d40438b74
Subhrodip Roy <roysubhrodiproy@gmail.com>
2025-04-29 23:45:10 +0530

    .py

---
+++ Array/Sorting/Insertion-Sorting/InsertionSort.py
@@ -0,0 +1,23 @@
+
+# Take input from the user
+size = int(input("Enter the size of the array: "))
+arr = []
+
+print("Enter the elements of the array:")
+for _ in range(size):
+    arr.append(int(input()))
+
+# Print original array
+print("Original array:", arr)
+
+# Perform insertion sort
+for i in range(1, size):
+    key = arr[i]
+    j = i - 1
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+    arr[j + 1] = key
+
+# Print sorted array
+print("Sorted array:", arr)
